b726a08159daf0d6ceab56d1a400666fe614064b
Karthikeya-Akhandam <191661911+Karthikeya-Akhandam@users.noreply.github.com>
2024-12-28 12:34:19 +0530

    Update AutomateActivityTests.java

--- AutomateActivityTests.java
+++ AutomateActivityTests.java
@@ -49,6 +49,12 @@ public class AutomateActivityTests {
                 .skip(1) // Skip the header row
                 .map(line -> DynamicTest.dynamicTest("Activity test for " + line[0] + " " + line[1],
                         () -> {
+                            /*// Skip the third test (index 0 to 19 based on csv file index)
+                            if (lines.indexOf(line) <= 1 && lines.indexOf(line) <= 21) {
+                                System.out.println("Skipping test for " + line[0] + " " + line[1]);
+                                return; // Skip this test
+                            }
+                            //End of skip*/
                             testActivityProcess(
                                     line[0], line[1], line[2], line[3], line[4]
                             );
@@ -57,7 +63,7 @@ public class AutomateActivityTests {
     }
 
     private void testActivityProcess(String s, String s1, String s2, String s3, String s4) throws InterruptedException {
-        int t1 = Integer.parseInt(s2);
+        int time = Integer.parseInt(s2);
         int laps = Integer.parseInt(s3);
         int miss = Integer.parseInt(s4);
         Thread.sleep(1000);
@@ -65,167 +71,152 @@ public class AutomateActivityTests {
         switch (s) {
             case "Cricket" -> {
                 switch (s1) {
-                    case "Retrieval" -> activity2(driver, laps, 1, 1, 0);
-                    case "RH 4" -> activity(t1, driver, laps, 4, miss, 1, 0);
-                    case "RH 6" -> activity(t1, driver, laps, 2, miss, 1, 0);
-                    case "RW 4" -> activity(t1, driver, laps, 5, miss, 1, 0);
-                    case "RW 6" -> activity(t1, driver, laps, 3, miss, 1, 0);
-                    case "SH 4" -> activity(t1, driver, laps, 6, miss, 1, 0);
-                    case "SH 6" -> activity(t1, driver, laps, 2, miss, 1, 1);
-                    case "SW 4" -> activity(t1, driver, laps, 5, miss, 1, 1);
-                    case "SW 6" -> activity(t1, driver, laps, 3, miss, 1, 1);
+                    case "Retrieval" -> activity2(driver, s, laps, 0, 1);
+                    case "RH 4" -> activity(driver, s, time, laps, miss, 0, 4);
+                    case "RH 6" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "RW 4" -> activity(driver, s, time, laps, miss, 0, 5);
+                    case "RW 6" -> activity(driver, s, time, laps, miss, 0, 3);
+                    case "SH 4" -> activity(driver, s, time, laps, miss, 0, 6);
+                    case "SH 6" -> activity(driver, s, time, laps, miss, 1, 2);
+                    case "SW 4" -> activity(driver, s, time, laps, miss, 1, 5);
+                    case "SW 6" -> activity(driver, s, time, laps, miss, 1, 3);
                 }
             }
             case "Tennis" -> {
                 switch (s1) {
-                    case "RH 4" -> activity(t1, driver, laps, 1, miss, 2, 0);
-                    case "RH 6" -> activity(t1, driver, laps, 4, miss, 2, 0);
-                    case "RW 4" -> activity(t1, driver, laps, 2, miss, 2, 0);
-                    case "RW 6" -> activity(t1, driver, laps, 5, miss, 2, 0);
-                    case "SH 4" -> activity(t1, driver, laps, 3, miss, 2, 0);
-                    case "SH 6" -> activity(t1, driver, laps, 6, miss, 2, 0);
-                    case "SW 4" -> activity(t1, driver, laps, 3, miss, 2, 1);
-                    case "SW 6" -> activity(t1, driver, laps, 6, miss, 2, 1);
+                    case "RH 4" -> activity(driver, s, time, laps, miss, 0, 1);
+                    case "RH 6" -> activity(driver, s, time, laps, miss, 0, 4);
+                    case "RW 4" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "RW 6" -> activity(driver, s, time, laps, miss, 0, 5);
+                    case "SH 4" -> activity(driver, s, time, laps, miss, 0, 3);
+                    case "SH 6" -> activity(driver, s, time, laps, miss, 0, 6);
+                    case "SW 4" -> activity(driver, s, time, laps, miss, 1, 3);
+                    case "SW 6" -> activity(driver, s, time, laps, miss, 1, 6);
                 }
             }
             case "Badminton" -> {
                 switch (s1) {
-                    case "Six-Point Footwork Sequential" -> activity(t1, driver, laps, 1, miss, 3, 0);
-                    case "Six-Point Footwork Random" -> activity(t1, driver, laps, 4, miss, 3, 0);
-                    case "RH 4" -> activity(t1, driver, laps, 2, miss, 3, 0);
-                    case "RH 6" -> activity(t1, driver, laps, 5, miss, 3, 0);
-                    case "RW 4" -> activity(t1, driver, laps, 3, miss, 3, 0);
-                    case "RW 6" -> activity(t1, driver, laps, 6, miss, 3, 0);
-                    case "SH 4" -> activity(t1, driver, laps, 1, miss, 3, 2);
-                    case "SH 6" -> activity(t1, driver, laps, 4, miss, 3, 2);
-                    case "SW 4" -> activity(t1, driver, laps, 2, miss, 3, 2);
-                    case "SW 6" -> activity(t1, driver, laps, 5, miss, 3, 2);
-                    case "BD Hit 1" -> activity(t1, driver, laps, 3, miss, 3, 2);
-                    case "BD Wave 1" -> activity(t1, driver, laps, 6, miss, 3, 2);
+                    case "Six-Point Footwork Sequential" -> activity(driver, s, time, laps, miss, 0, 1);
+                    case "Six-Point Footwork Random" -> activity(driver, s, time, laps, miss, 0, 4);
+                    case "RH 4" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "RH 6" -> activity(driver, s, time, laps, miss, 0, 5);
+                    case "RW 4" -> activity(driver, s, time, laps, miss, 0, 3);
+                    case "RW 6" -> activity(driver, s, time, laps, miss, 0, 6);
+                    case "SH 4" -> activity(driver, s, time, laps, miss, 2, 1);
+                    case "SH 6" -> activity(driver, s, time, laps, miss, 2, 4);
+                    case "SW 4" -> activity(driver, s, time, laps, miss, 2, 2);
+                    case "SW 6" -> activity(driver, s, time, laps, miss, 2, 5);
+                    case "BD Hit 1" -> activity(driver, s, time, laps, miss, 2, 3);
+                    case "BD Wave 1" -> activity(driver, s, time, laps, miss, 2, 6);
                 }
             }
             case "School Fitness" -> {
                 switch (s1) {
-                    case "RH 4" -> activity(t1, driver, laps, 1, miss, 4, 0);
-                    case "RH 6" -> activity(t1, driver, laps, 4, miss, 4, 0);
-                    case "RW 4" -> activity(t1, driver, laps, 2, miss, 4, 0);
-                    case "RW 6" -> activity(t1, driver, laps, 5, miss, 4, 0);
-                    case "SH 4" -> activity(t1, driver, laps, 3, miss, 4, 0);
-                    case "SH 6" -> activity(t1, driver, laps, 6, miss, 4, 0);
-                    case "SW 4" -> activity(t1, driver, laps, 3, miss, 4, 1);
-                    case "SW 6" -> activity(t1, driver, laps, 6, miss, 4, 1);
+                    case "RH 4" -> activity(driver, s, time, laps, miss, 0, 1);
+                    case "RH 6" -> activity(driver, s, time, laps, miss, 0, 4);
+                    case "RW 4" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "RW 6" -> activity(driver, s, time, laps, miss, 0, 5);
+                    case "SH 4" -> activity(driver, s, time, laps, miss, 0, 3);
+                    case "SH 6" -> activity(driver, s, time, laps, miss, 0, 6);
+                    case "SW 4" -> activity(driver, s, time, laps, miss, 1, 3);
+                    case "SW 6" -> activity(driver, s, time, laps, miss, 1, 6);
                 }
             }
             case "Soccer" -> {
                 switch (s1) {
-                    case "RH 4" -> activity(t1, driver, laps, 1, miss, 5, 0);
-                    case "RH 6" -> activity(t1, driver, laps, 4, miss, 5, 0);
-                    case "RW 4" -> activity(t1, driver, laps, 2, miss, 5, 0);
-                    case "RW 6" -> activity(t1, driver, laps, 5, miss, 5, 0);
-                    case "SH 4" -> activity(t1, driver, laps, 3, miss, 5, 0);
-                    case "SH 6" -> activity(t1, driver, laps, 6, miss, 5, 0);
-                    case "SW 4" -> activity(t1, driver, laps, 3, miss, 5, 1);
-                    case "SW 6" -> activity(t1, driver, laps, 6, miss, 5, 1);
+                    case "RH 4" -> activity(driver, s, time, laps, miss, 0, 1);
+                    case "RH 6" -> activity(driver, s, time, laps, miss, 0, 4);
+                    case "RW 4" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "RW 6" -> activity(driver, s, time, laps, miss, 0, 5);
+                    case "SH 4" -> activity(driver, s, time, laps, miss, 0, 3);
+                    case "SH 6" -> activity(driver, s, time, laps, miss, 0, 6);
+                    case "SW 4" -> activity(driver, s, time, laps, miss, 1, 3);
+                    case "SW 6" -> activity(driver, s, time, laps, miss, 1, 6);
                 }
             }
             case "Physiotherapy" -> {
                 switch (s1) {
-                    case "PT Sequential Laps" -> activity2(driver, laps, 1, 6, 0);
-                    case "RH 6" -> activity(t1, driver, laps, 4, miss, 6, 0);
-                    case "RW 6" -> activity(t1, driver, laps, 2, miss, 6, 0);
-                    case "SH 6" -> activity(t1, driver, laps, 5, miss, 6, 0);
-                    case "SW 6" -> activity(t1, driver, laps, 3, miss, 6, 0);
-                    case "Sequential Circle Hit 4" -> activity(t1, driver, laps, 6, miss, 6, 0);
-                    case "Sequential Circle Wave 4" -> activity2(driver, laps, 2, 6, 1);
-                    case "Sequential Circle Hit 6" -> activity2(driver, laps, 6, 6, 1);
-                    case "Sequential Circle Wave 6" -> activity2(driver,laps,3, 6, 1);
-                    case "T Test" -> activity2(driver, laps, 7, 6, 1);
-                    case "3 Cone Wave" -> activity2(driver, laps, 4, 6, 1);
-                    case "10 Meter" -> activity2(driver, laps, 8, 6, 1);
-                    case "SEMO Wave" -> activity2(driver, laps, 2, 6, 2);
-                    case "Pod Colour 4" -> activity2(driver, laps, 5, 6, 2);
-                    case "Pod Colour 6" -> activity2(driver, laps, 3, 6, 2);
+                    case "PT Sequential Laps" -> activity2(driver, s, laps, 0, 1);
+                    case "RH 6" -> activity(driver, s, time, laps, miss, 0, 4);
+                    case "RW 6" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "SH 6" -> activity(driver, s, time, laps, miss, 0, 5);
+                    case "SW 6" -> activity(driver, s, time, laps, miss, 0, 3);
+                    case "Sequential Circle Hit 4" -> activity(driver, s, time, laps, miss, 0, 6);
+                    case "Sequential Circle Wave 4" -> activity2(driver, s, laps, 1, 2);
+                    case "Sequential Circle Hit 6" -> activity2(driver, s, laps, 1, 6);
+                    case "Sequential Circle Wave 6" -> activity2(driver, s, laps, 1, 3);
+                    case "T Test" -> activity2(driver, s, laps, 1, 7);
+                    case "3 Cone Wave" -> activity2(driver, s, laps, 1, 4);
+                    case "10 Meter" -> activity2(driver, s, laps, 1, 8);
+                    case "SEMO Wave" -> activity2(driver, s, laps, 2, 2);
+                    case "Pod Colour 4" -> activity2(driver, s, laps, 2, 5);
+                    case "Pod Colour 6" -> activity2(driver, s, laps, 2, 3);
                 }
             }
             case "Military Training" -> {
-                driver.findElement(AppiumBy.androidUIAutomator(
-                        "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(10)"
-                ));
                 switch (s1) {
-                    case "RH 4" -> activity(t1, driver, laps, 1, miss, 1, 0);
-                    case "RH 6" -> activity(t1, driver, laps, 4, miss, 1, 0);
-                    case "RW 4" -> activity(t1, driver, laps, 2, miss, 1, 0);
-                    case "RW 6" -> activity(t1, driver, laps, 5, miss, 1, 0);
-                    case "SH 4" -> activity(t1, driver, laps, 3, miss, 1, 0);
-                    case "SH 6" -> activity(t1, driver, laps, 6, miss, 1, 0);
-                    case "SW 4" -> activity(t1, driver, laps, 3, miss, 1, 1);
-                    case "SW 6" -> activity(t1, driver, laps, 6, miss, 1, 1);
+                    case "RH 4" -> activity(driver, s, time, laps, miss, 0, 1);
+                    case "RH 6" -> activity(driver, s, time, laps, miss, 0, 4);
+                    case "RW 4" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "RW 6" -> activity(driver, s, time, laps, miss, 0, 5);
+                    case "SH 4" -> activity(driver, s, time, laps, miss, 0, 3);
+                    case "SH 6" -> activity(driver, s, time, laps, miss, 0, 6);
+                    case "SW 4" -> activity(driver, s, time, laps, miss, 1, 3);
+                    case "SW 6" -> activity(driver, s, time, laps, miss, 1, 6);
                 }
             }
             case "Boxing" -> {
-                driver.findElement(AppiumBy.androidUIAutomator(
-                        "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(10)"
-                ));
                 switch (s1) {
-                    case "SH 6" -> activity(t1, driver, laps, 1, miss, 2, 0);
-                    case "SH 4" -> activity(t1, driver, laps, 3, miss, 2, 0);
-                    case "RH 6" -> activity(t1, driver, laps, 2, miss, 2, 0);
-                    case "RH 4" -> activity(t1, driver, laps, 4, miss, 2, 0);
+                    case "SH 6" -> activity(driver, s, time, laps, miss, 0, 1);
+                    case "SH 4" -> activity(driver, s, time, laps, miss, 0, 3);
+                    case "RH 6" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "RH 4" -> activity(driver, s, time, laps, miss, 0, 4);
                 }
             }
             case "Auditory Reflex Training" -> {
-                driver.findElement(AppiumBy.androidUIAutomator(
-                        "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(10)"
-                ));
                 switch (s1) {
-                    case "SW 4" -> activity(t1, driver, laps, 1, miss, 3, 0);
-                    case "SW 6" -> activity(t1, driver, laps, 3, miss, 3, 0);
-                    case "RW 4" -> activity(t1, driver, laps, 2, miss, 3, 0);
-                    case "RW 6" -> activity(t1, driver, laps, 4, miss, 3, 0);
+                    case "SW 4" -> activity(driver, s, time, laps, miss, 0, 1);
+                    case "SW 6" -> activity(driver, s, time, laps, miss, 0, 3);
+                    case "RW 4" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "RW 6" -> activity(driver, s, time, laps, miss, 0, 4);
                 }
             }
             case "Audiblaze Reflex Training" -> {
-                driver.findElement(AppiumBy.androidUIAutomator(
-                        "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(10)"
-                ));
                 switch (s1) {
-                    case "SW 4" -> activity(t1, driver, laps, 1, miss, 4, 0);
-                    case "SW 6" -> activity(t1, driver, laps, 3, miss, 4, 0);
-                    case "RW 4" -> activity(t1, driver, laps, 2, miss, 4, 0);
-                    case "RW 6" -> activity(t1, driver, laps, 4, miss, 4, 0);
+                    case "SW 4" -> activity(driver, s, time, laps, miss, 0, 1);
+                    case "SW 6" -> activity(driver, s, time, laps, miss, 0, 3);
+                    case "RW 4" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "RW 6" -> activity(driver, s, time, laps, miss, 0, 4);
                 }
             }
             case "New Activities" -> {
-                driver.findElement(AppiumBy.androidUIAutomator(
-                        "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(10)"
-                ));
                 switch (s1) {
-                    case "FH 6" -> activity(t1, driver, laps, 1, miss, 5, 0);
-                    case "FW 6" -> activity(t1, driver, laps, 4, miss, 5, 0);
-                    case "OH 6" -> activity(t1, driver, laps, 2, miss, 5, 0);
-                    case "OW 6" -> activity(t1, driver, laps, 5, miss, 5, 0);
-                    case "Back to home Hit 6" -> activity(t1, driver, laps, 3, miss, 5, 0);
+                    case "FH 6" -> activity(driver, s, time, laps, miss, 0, 1);
+                    case "FW 6" -> activity(driver, s, time, laps, miss, 0, 4);
+                    case "OH 6" -> activity(driver, s, time, laps, miss, 0, 2);
+                    case "OW 6" -> activity(driver, s, time, laps, miss, 0, 5);
+                    case "Back to home Hit 6" -> activity(driver, s, time, laps, miss, 0, 3);
                 }
             }
             case "Table Tennis" -> {
-                driver.findElement(AppiumBy.androidUIAutomator(
-                        "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(10)"
-                ));
                 if (s1.equals("X Drill")) {
-                    activity2(driver, laps, 1, 6, 0);
+                    activity2(driver, s, laps, 0, 1);
                 }
             }
         }
     }
 
-    public static void activity(int time, AndroidDriver driver, int laps, int index2, int miss, int index, int index1) {
+    public static void activity(AndroidDriver driver,String s, int time, int laps, int miss, int index1, int index2) {
 
         //Want the test case
         String opt = JOptionPane.showInputDialog("Do you want this test case : ");
         if (opt.equalsIgnoreCase("N")) {
             return;
         }
-        driver.findElement(By.xpath("(//android.widget.ImageView[@resource-id=\"com.dashpod.sportsandfitness:id/imageView11\"])[" + index + "]")).click();
+
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id=\"com.dashpod.sportsandfitness:id/autoCompleteTextUpdate\"]")).clear();
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id=\"com.dashpod.sportsandfitness:id/autoCompleteTextUpdate\"]")).sendKeys(s);
+        driver.findElement(By.xpath("//android.widget.ImageView[@resource-id=\"com.dashpod.sportsandfitness:id/imageView11\"]")).click();
 
         System.out.println("Scroll : " + index1);
         if (index1 == 1) scroll(driver);
@@ -309,14 +300,18 @@ public class AutomateActivityTests {
         }
     }
 
-    public static void activity2(AndroidDriver driver, int laps, int index2, int index, int index1) throws InterruptedException {
+    public static void activity2(AndroidDriver driver, String s, int laps, int index1, int index2) throws InterruptedException {
 
         //Want the test case
         String opt = JOptionPane.showInputDialog("Do you want this test case : ");
         if (opt.equalsIgnoreCase("N")) {
             return;
         }
-        driver.findElement(By.xpath("(//android.widget.ImageView[@resource-id=\"com.dashpod.sportsandfitness:id/imageView11\"])[" + index + "]")).click();
+
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id=\"com.dashpod.sportsandfitness:id/autoCompleteTextUpdate\"]")).clear();
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id=\"com.dashpod.sportsandfitness:id/autoCompleteTextUpdate\"]")).sendKeys(s);
+        driver.findElement(By.xpath("//android.widget.ImageView[@resource-id=\"com.dashpod.sportsandfitness:id/imageView11\"]")).click();
+
         if (index1 == 0) {
 
         } else if (index1 == 1) {
